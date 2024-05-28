12a99fc8ae8282d96db29bb2c028c9143f3444a8
abbassimedayoub <abbassimedayoub@gamil.com>
2024-05-28 11:39:26 +0200

    add

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-"""list all documents in a collection"""
-
-from pymongo import MongoClient
+""" good morning"""
 
 
 def list_all(mongo_collection):
-    """Return a list of docs if not empty"""
-    result = mongo_collection.school.find()
-    if result:
-        return result
-    return []
+    """ o_o """
+    return list(mongo_collection.find())
