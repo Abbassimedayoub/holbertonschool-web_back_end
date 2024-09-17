037afa0343fd124b9f98df81bae22961322c7152
abbassimedayoub <abbassimedayoub@gamil.com>
2024-09-17 17:28:46 +0200

    x+

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -5,12 +5,13 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """Basic caching system"""
+
     def put(self, key, item):
+        """Puts an item in cache"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """Gets item from cache"""
+        return self.cache_data.get(key, None)
